28cfb3ecc6e02c4b7eba1968044b1b41006fd809
marzim <marvin.casagnap@gmail.com>
2016-03-12 20:00:43 +0800

    commit changes

--- app/assets/javascripts/connectfour.js
+++ app/assets/javascripts/connectfour.js
@@ -1,200 +1,78 @@
-var gameField = new Array();
-var board = document.getElementById("game-table");
-alert("board " + board);
-var currentCol;
-var currentRow;
-var currentPlayer;
-var id = 1;
+(function (doc, win, onclick, gid, classname, content, showMessage) {
+    var
+            a, b, c, colorLabel, cid, players, current, finished, newgameLabel, wonLabel, laststart = 1,
+            cellAt = function (i, j) {
+                return doc[gid](cid + i + j);
+            },
+            isCurrentColor = function (i, j) {
+                return cellAt(i, j)[classname] === players[current];
+            },
+            start = function () {
+                current = laststart = (laststart + 1) % 2;
+                finished = 0;
+                colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2];
+                for (a = 1; a < 7; a++)
+                    for (b = 1; b < 8; b++)
+                        cellAt(a, b)[classname] = '';
+            },
+            makeMove = function (i, j, s) {
+                s > 0 && (cellAt(s, j)[classname] = '');
+                cellAt(s + 1, j)[classname] = players[current];
+                s === i - 1 ? function (i, j) {
+                    return function (i, j) {
+                        for (a = j - 1; 0 < a && isCurrentColor(i, a); a--) {
+                        }
+                        for (b = j + 1; 8 > b && isCurrentColor(i, b); b++) {
+                        }
+                        return 4 < b - a;
+                    }(i, j) || function (i, j) {
+                        for (c = i + 1; 7 > c && isCurrentColor(c, j); c++) {
+                        }
+                        return 3 < c - i;
+                    }(i, j) || function (i, j) {
+                        for (a = i - 1, b = j - 1; 0 < a && !(1 > b) && isCurrentColor(a, b); a--)
+                            b--;
+                        for (c = i + 1, b = j + 1; 7 > c && !(7 < b) && isCurrentColor(c, b); c++)
+                            b++;
+                        return 4 < c - a
+                    }(i, j) || function (i, j) {
+                        for (a = i - 1, b = j + 1; 0 < a && !(7 < b) && isCurrentColor(a, b); a--)
+                            b++;
+                        for (c = i + 1, b = j - 1; 7 > c && !(1 > b) && isCurrentColor(c, b); c++)
+                            b--;
+                        return 4 < c - a;
+                    }(i, j);
+                }(i, j)
+                        ? finished = 1 && win[showMessage](doc[gid](wonLabel)[content].replace("%s", players[current].toLowerCase())) && start()
+                        : colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2]
+                        : setTimeout(function () {
+                            makeMove(i, j, s + 1)
+                        }, 20);
 
-newgame();
+            };
 
-function newgame(){
-
-  prepareField();
-  var currPlayer = Math.floor(Math.random()*2+1);
-  alert("curr player: " + currPlayer);
-  placeDisc(currPlayer);
-  
-}
-
-function checkForVictory(row,col){
-  if(getAdj(row,col,0,1)+getAdj(row,col,0,-1) > 2){
-    return true;
-  } else {
-    if(getAdj(row,col,1,0) > 2){
-      return true;
-    } else {
-      if(getAdj(row,col,-1,1)+getAdj(row,col,1,-1) > 2){
-        return true;
-      } else {
-        if(getAdj(row,col,1,1)+getAdj(row,col,-1,-1) > 2){
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
-  }
-}
-
-function getAdj(row,col,row_inc,col_inc){
-  if(cellVal(row,col) == cellVal(row+row_inc,col+col_inc)){
-    return 1+getAdj(row+row_inc,col+col_inc,row_inc,col_inc);
-  } else {
-    return 0;
-  }
-}
-
-function cellVal(row,col){
-  if(gameField[row] == undefined || gameField[row][col] == undefined){
-    return -1;
-  } else {
-    return gameField[row][col];
-  }
-}
-
-function firstFreeRow(col,player){
-  for(var i = 0; i<6; i++){
-    if(gameField[i][col]!=0){
-      break;
-    }
-  }
-  gameField[i-1][col] = player;
-  return i-1;
-}
-
-function possibleColumns(){
-  var moves_array = new Array();
-  for(var i=0; i<7; i++){
-    if(gameField[0][i] == 0){
-      moves_array.push(i);
-    }
-  }
-  return moves_array;
-}
-
-function think(){
-  var possibleMoves = possibleColumns();
-  var aiMoves = new Array();
-  var blocked;
-  var bestBlocked = 0;
-  
-  for(var i=0; i<possibleMoves.length; i++){
-    for(var j=0; j<6; j++){
-      if(gameField[j][possibleMoves[i]] != 0){
-        break;
-      }
-    }
-    
-    gameField[j-1][possibleMoves[i]] = 1;
-    blocked = getAdj(j-1,possibleMoves[i],0,1)+getAdj(j-1,possibleMoves[i],0,-1);
-    blocked = Math.max(blocked,getAdj(j-1,possibleMoves[i],1,0));
-    blocked = Math.max(blocked,getAdj(j-1,possibleMoves[i],-1,1));
-    blocked = Math.max(blocked,getAdj(j-1,possibleMoves[i],1,1)+getAdj(j-1, possibleMoves[i],-1,-1));
-    
-    if(blocked >= bestBlocked){
-      if(blocked>bestBlocked){
-        bestBlocked = blocked;
-        aiMoves = new Array();
-      }
-      aiMoves.push(possibleMoves[i]);
-    }
-    gameField[j-1][possibleMoves[i]] = 0;
-  }
-  
-  return aiMoves;
-}
-
-function Disc(player){
-  this.player = player;
-  this.color = player == 1 ? 'red' : 'yellow';
-  this.id = id.toString();
-  id++;
-  
-  this.addToScene = function(){
-    board.innerHTML += '<div id="d'+this.id+'" class="disc '+this.color+'"></div>';
-	alert("Disc:addToScene currentPlayer: " + currentPlayer);
-    if(currentPlayer==2){
-      //computer move
-      var possibleMoves = think();
-      var cpuMove = Math.floor( Math.random() * possibleMoves.length);
-      currentCol = possibleMoves[cpuMove];
-      document.getElementById('d'+this.id).style.left = (14+60*currentCol)+"px";
-      dropDisc(this.id,currentPlayer);
-    }
-  }
-  
-  var $this = this;
-  document.onmousemove = function(evt){
-    alert("document.onmousemove currentPlayer: " + currentPlayer); 
-    if(currentPlayer == 1){
-    currentCol = Math.floor((evt.clientX - board.offsetLeft)/60);
-	
-	if(currentCol<0){currentCol=0;}
-    if(currentCol>6){currentCol=6;}
-    document.getElementById('d'+$this.id).style.left = (14+60*currentCol)+"px";
-    document.getElementById('d'+$this.id).style.top = "-55px";
-    }
-  }
-  document.onload = function(evt){
-    if(currentPlayer == 1){
-    currentCol = Math.floor((evt.clientX - board.offsetLeft)/60);
-	alert("document.onload currentcol: " + currentCol);
-    if(currentCol<0){currentCol=0;}
-    if(currentCol>6){currentCol=6;}
-    document.getElementById('d'+$this.id).style.left = (14+60*currentCol)+"px";
-    document.getElementById('d'+$this.id).style.top = "-55px";
-    }
-  }
-  
-  document.onclick = function(evt){
-    if(currentPlayer == 1){
-	alert("currentcol: " + currentCol);
-      if(possibleColumns().indexOf(currentCol) != -1){
-        dropDisc($this.id,$this.player);
-      }
-    }
-	alert("current player: " + currentPlayer);
-  }
-}
-
-function dropDisc(cid,player){
-  currentRow = firstFreeRow(currentCol,player);
-  alert("current row: " + currentRow);
-  moveit(cid,(14+currentRow*60));
-  currentPlayer = player;
-  alert("current player: " + currentPlayer);
-  checkForMoveVictory();
-}
-
-function checkForMoveVictory(){
-  if(!checkForVictory(currentRow,currentCol)){
-    placeDisc(3-currentPlayer);
-  } else {
-    var ww = currentPlayer == 2 ? 'Computer' : 'Player';
-    placeDisc(3-currentPlayer);
-    alert(ww+" win!");
-    board.innerHTML = "";
-    newgame();
-  }
-}
-
-function placeDisc(player){
-  currentPlayer = player;
-  var disc = new Disc(player);
-  disc.addToScene();
-}
-
-function prepareField(){
-  gameField = new Array();
-  for(var i=0; i<6; i++){
-	gameField[i] = new Array();
-    for(var j=0; j<7; j++){
-	  gameField[i].push(0);
-    }
-  }
-}
-
-function moveit(who,where){
-    document.getElementById('d'+who).style.top = where+'px';
-}
+    return function (n, w, c, h, p1, p2) {
+        cid = c;
+        newgameLabel = n;
+        wonLabel = w;
+        colorLabel = doc[gid](c);
+        players = [doc[gid](p1)[content], doc[gid](p2)[content]];
+        for (a = 1; a < 7; a++)
+            for (b = 1; b < 8; b++)
+                cellAt(a, b)[onclick] = function (b, a) {
+                    return function () {
+                        if (!finished)
+                            for (a = 6; a > 0; a--)
+                                if (!cellAt(a, b)[classname]) {
+                                    makeMove(a, b, 0);
+                                    break;
+                                }
+                    };
+                }(b);
+        ;
+        doc[gid](h)[onclick] = function () {
+            win[showMessage](doc[gid](newgameLabel)[content]) && start()
+        };
+        start();
+    };
+})(document, window, "onclick", "getElementById", "className", "innerHTML", "confirm")("newgame", "won", "color", "restart", "p1", "p2");
