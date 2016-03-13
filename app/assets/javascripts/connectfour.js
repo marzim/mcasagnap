ec215ed7f59c2a9e2e722bc9dbbe5c3833cacac1
marzim <marvin.casagnap@gmail.com>
2016-03-14 00:31:25 +0800

    commit new changes

--- app/assets/javascripts/connectfour.js
+++ app/assets/javascripts/connectfour.js
@@ -1,13 +1,16 @@
 (function (doc, win, onclick, gid, classname, content, showMessage) {
     var
             a, b, c, colorLabel, cid, players, current, finished, newgameLabel, wonLabel, laststart = 1,
-            cellAt = function (i, j) {
-                return doc[gid](cid + i + j);
+  
+			cellAt = function (row, column) {
+                return doc[gid](cid + row + column);
             },
-            isCurrentColor = function (i, j) {
-                return cellAt(i, j)[classname] === players[current];
+            
+			isCurrentColor = function (row, column) {
+                return cellAt(row, column)[classname] === players[current];
             },
-            start = function () {
+            
+			start = function () {
                 current = laststart = (laststart + 1) % 2;
                 finished = 0;
                 colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2];
@@ -15,38 +18,47 @@
                     for (b = 1; b < 8; b++)
                         cellAt(a, b)[classname] = '';
             },
-            makeMove = function (i, j, s) {
-                s > 0 && (cellAt(s, j)[classname] = '');
-                cellAt(s + 1, j)[classname] = players[current];
-                s === i - 1 ? function (i, j) {
-                    return function (i, j) {
-                        for (a = j - 1; 0 < a && isCurrentColor(i, a); a--) {
+            
+			makeMove = function (row, column, previousCell) {
+				previousCell > 0 && (cellAt(previousCell, column)[classname] = '');
+                
+				cellAt(previousCell + 1, column)[classname] = players[current];
+                previousCell === row - 1 ? 
+				function (row, column) {
+                    return function (row, column) {
+                        for (a = column - 1; 0 < a && isCurrentColor(row, a); a--) {
                         }
-                        for (b = j + 1; 8 > b && isCurrentColor(i, b); b++) {
+                        for (b = column + 1; 8 > b && isCurrentColor(row, b); b++) {
                         }
                         return 4 < b - a;
-                    }(i, j) || function (i, j) {
-                        for (c = i + 1; 7 > c && isCurrentColor(c, j); c++) {
+                    }
+					
+					(row, column) || function (row, column) {
+                        for (c = row + 1; 7 > c && isCurrentColor(c, column); c++) {
                         }
-                        return 3 < c - i;
-                    }(i, j) || function (i, j) {
-                        for (a = i - 1, b = j - 1; 0 < a && !(1 > b) && isCurrentColor(a, b); a--)
+                        return 3 < c - row;
+                    }
+					
+					(row, column) || function (row, column) {
+                        for (a = row - 1, b = column - 1; 0 < a && !(1 > b) && isCurrentColor(a, b); a--)
                             b--;
-                        for (c = i + 1, b = j + 1; 7 > c && !(7 < b) && isCurrentColor(c, b); c++)
+                        for (c = row + 1, b = column + 1; 7 > c && !(7 < b) && isCurrentColor(c, b); c++)
                             b++;
                         return 4 < c - a
-                    }(i, j) || function (i, j) {
-                        for (a = i - 1, b = j + 1; 0 < a && !(7 < b) && isCurrentColor(a, b); a--)
+                    }
+					
+					(row, column) || function (row, column) {
+                        for (a = row - 1, b = column + 1; 0 < a && !(7 < b) && isCurrentColor(a, b); a--)
                             b++;
-                        for (c = i + 1, b = j - 1; 7 > c && !(1 > b) && isCurrentColor(c, b); c++)
+                        for (c = row + 1, b = column - 1; 7 > c && !(1 > b) && isCurrentColor(c, b); c++)
                             b--;
                         return 4 < c - a;
-                    }(i, j);
-                }(i, j)
-                        ? finished = 1 && win[showMessage](doc[gid](wonLabel)[content].replace("%s", players[current].toLowerCase())) && start()
-                        : colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2]
-                        : setTimeout(function () {
-                            makeMove(i, j, s + 1)
+                    }(row, column);
+                }
+					(row, column) ? finished = 1 && win[showMessage](doc[gid](wonLabel)[content].replace("%s", players[current].toLowerCase())) && start()
+							: colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2]
+				: setTimeout(function () {
+                            makeMove(row, column, previousCell + 1)
                         }, 20);
 
             };
@@ -75,4 +87,5 @@
         };
         start();
     };
-})(document, window, "onclick", "getElementById", "className", "innerHTML", "confirm")("newgame", "won", "color", "restart", "p1", "p2");
+})
+(document, window, "onclick", "getElementById", "className", "innerHTML", "confirm")("newgame", "won", "color", "restart", "p1", "p2");
